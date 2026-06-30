f7060cc932546b1aae68056aa72a16da865a31d8
neeraj-meena-tcgls <neeraj.meena@tcgre.com>
2026-06-30 18:41:26 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("h1", { id: "heading" }, "jj nnn");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
+
+console.log(root);
